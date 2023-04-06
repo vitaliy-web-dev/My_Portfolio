e7f39d54d2de5479c4dd484fa7d1d02b46bc1373
vitaliy-web-dev <95997245+vitaliy-web-dev@users.noreply.github.com>
2023-04-06 22:58:28 +0300

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,15 @@
+
+const Contact = () => {
+    
+    
+    return(
+        <>
+            <div className='header'>
+                <h1>Contact</h1>
+                <h1><strong>096-749-52-54</strong></h1>       
+            </div>
+           
+        </>
+    )
+ }    
+export default Contact;
