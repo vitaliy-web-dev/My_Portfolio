e6ff7373b85f9c483b649f7401f31a3a636c6050
vitaliy-web-dev <95997245+vitaliy-web-dev@users.noreply.github.com>
2023-04-06 22:57:53 +0300

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'; 
+
+const Header = () => {
+
+    return(
+        <>
+            <div className='header'>
+                <h1>Home</h1>               
+            </div>
+        </>
+    )
+}
+
+export default Header;
